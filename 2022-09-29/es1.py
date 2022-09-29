d3b5c51d712d71b9aca65e99765eff758e992981
Cobbleston <canovasimo@gmail.com>
2022-09-29 15:11:54 +0200

    first lesson

---
+++ 2022-09-29/es1.py
@@ -0,0 +1,69 @@
+import matplotlib as mpl
+import numpy as np
+import math as m
+
+#c = 2.3 + 4.1j
+#print(c.real)
+#print(c.imag)
+
+print()
+
+#print("hello", "world")
+#print("hello", "world", sep="", end="\t")
+#print("non a capo")
+
+print()
+
+#print(3/2)
+#print(3//2)
+
+print()
+
+#l = [i*2 for i in range(0, 50) if (i % 3 == 0)]
+#print(l)
+
+print()
+
+#A = np.array([[1, 2, 3], [4, 5, 6]])
+#print("A: \n", A)
+#print(A.T)
+#print(A.size, A.shape, A.dtype, sep="\t")
+#print(np.reshape(A, (6, 1)))
+
+print()
+
+#print(np.zeros((3, 2), dtype=int))
+#print(np.ones((2, 5)))
+#print(np.identity(4))
+
+print()
+
+#x = np.array([1, 2, 3, 4, 5, 6])
+#y = np.array([6, 5, 4, 3, 2, 1])
+#y = np.reshape(y, (6, 1))
+#print(x)
+#print(y)
+#print(x+y)
+#print(x-y)
+#print(x*y)
+#print(x/y)
+#print(x//y)
+#print(x @ y)
+
+print()
+
+#A = np.random.randint(2, 20, (5, 5))
+#print(A)
+#print(np.sum(A, axis = 1))
+
+print()
+
+#A = np.random.randint(2, 20, (5, 5))
+#B = A[1:3, 1:4]
+#print(B)
+
+print()
+
+#C = np.arange(9)
+#C = np.reshape(C, (3, 3))
+#print(C)
